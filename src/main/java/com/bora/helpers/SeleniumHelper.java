509e2823e9cce8a0fc8149f3f0ab0548eb00ca54
nebulaButCute <88518472+nebulaButCute@users.noreply.github.com>
2021-09-14 09:48:13 -0400

    pre-hw add ins

--- src/main/java/com/bora/helpers/SeleniumHelper.java
+++ src/main/java/com/bora/helpers/SeleniumHelper.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;

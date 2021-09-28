87c354928d6dab5f54d944f02641b8c1ce7ec178
Nurzat Ejat <john.parhat@gmail.com>
2021-09-28 16:29:33 -0400

    'sds'

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -14,10 +14,11 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import com.bora.helpers.SeleniumHelper;
-import com.bora.managers.PageObjectManager;
-import com.bora.managers.WebDriverManager;
-
-import cucumber.TestContext;
+//import com.bora.managers.PageObjectManager;
+//import com.bora.managers.WebDriverManager;
+//import com.bora.pages.BasePage;
+//
+//import cucumber.TestContext;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
@@ -26,41 +27,65 @@ import io.cucumber.java.Scenario;
 
 //import cucumber.TestContext;
 
-public class Hooks {
+public class Hooks extends A{
 
-	TestContext testContext ;
+	public static void main(String[] args) {
+		 A a  =  new Hooks();
+		 System.out.println(a.getText());
+		 a.hello();
+	}
+//	TestContext testContext ;
 	
-	public Hooks(TestContext testContext){
-		this.testContext = testContext;
-	}
+//	public Hooks(TestContext testContext){
+//		this.testContext = testContext;
+//	}
 
-	@Before
-	public void setupTest() throws IOException  {
-
-		//Go to face book website
-		WebDriverManager driverManager = testContext.getDriverManager();
-		WebDriver driver = driverManager.getDriver();
-		driver.get("https://www.facebook.com");
-		SeleniumHelper.takeScreenshot(driver, "facebookhome");
-		System.out.println();
-	}
-
-	@After
-	public void endTest() {
-
-		testContext.getDriverManager().closeDriver();
-	}
-	
-	@AfterStep
-	public void afterStep(Scenario scenario) {
+	@Override
+	void hello() {
+		System.out.println("Hello from hooks");
 		
-		if(scenario.isFailed()) {
-			byte[] bytes = SeleniumHelper.takeScreenshot(testContext.getDriverManager().getDriver());
-			scenario.attach(bytes, "image/png", "image");
-		}
-		
-	
 	}
-	
 
+//	@Before
+//	public void setupTest() throws IOException  {
+//
+//		//Go to face book website
+//		WebDriverManager driverManager = testContext.getDriverManager();
+//		WebDriver driver = driverManager.getDriver();
+//		driver.get("https://www.facebook.com");
+//		SeleniumHelper.takeScreenshot(driver, "facebookhome");
+//		System.out.println();
+//	}
+//
+//	@After
+//	public void endTest() {
+//
+//		testContext.getDriverManager().closeDriver();
+//	}
+//	
+//	@AfterStep
+//	public void afterStep(Scenario scenario) {
+//		
+//		if(scenario.isFailed()) {
+//			byte[] bytes = SeleniumHelper.takeScreenshot(testContext.getDriverManager().getDriver());
+//			scenario.attach(bytes, "image/png", "image");
+//		}
+//		
+//	
+//	}
+//	
+
+	
+	
+	
 }
+
+abstract class A {
+	abstract void hello();
+	
+	public String getText() {
+		return "Hello World";
+	}
+}
+
+
